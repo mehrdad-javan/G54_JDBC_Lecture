03ef271674a150dae0ec2392c9235313e6e3a642
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-03-17 09:55:08 +0100

    use try-with-resources for ResultSet interface

--- src/main/java/se/lexicon/JdbcDemo.java
+++ src/main/java/se/lexicon/JdbcDemo.java
@@ -41,25 +41,28 @@ public class JdbcDemo {
 
     }
 
-    public static void  ex2(){
+    public static void ex2() {
         try (
                 Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/student_db", "root", "root");
                 PreparedStatement preparedStatement = connection.prepareStatement("SELECT id, name, class_group, create_date FROM student WHERE class_group LIKE ?");
         ) {
 
             preparedStatement.setString(1, "G1");
-            ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
-                String classGroup = resultSet.getString("class_group");
-                LocalDateTime createDateTime = resultSet.getTimestamp("create_date").toLocalDateTime();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    String classGroup = resultSet.getString("class_group");
+                    LocalDateTime createDateTime = resultSet.getTimestamp("create_date").toLocalDateTime();
 
-                String formattedDate = createDateTime.format(DateTimeFormatter.ofPattern("EEEE MMMM dd yyyy"));
+                    String formattedDate = createDateTime.format(DateTimeFormatter.ofPattern("EEEE MMMM dd yyyy"));
 
-                System.out.println("Id: " + id + " | Name: " + name + " | Class Group: " + classGroup + " | Date Time: " + formattedDate);
+                    System.out.println("Id: " + id + " | Name: " + name + " | Class Group: " + classGroup + " | Date Time: " + formattedDate);
+                }
             }
+
+
         } catch (SQLException e) {
             System.out.println("Error connecting to the database :" + e.getMessage());
         }
